3819884e835b083f249e97b413b2800360eec99d
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-10-27 21:16:08 +0300

    fix folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import { Layout } from "./Layout/Layout";
 import { lazy } from "react";
 
-const Home = lazy(() => import("../pages/Home"));
-const MovieDetails = lazy(() => import("../pages/MovieDetails"));
-const Movies = lazy(() => import("../pages/Movies"));
+const Home = lazy(() => import("../pages/Home/Home"));
+const MovieDetails = lazy(() => import("../pages/MovieDetails/MovieDetails"));
+const Movies = lazy(() => import("../pages/Movies/Movies"));
 const Cast = lazy(() => import("./Cast/Cast"));
 const Reviews = lazy(() => import("./Reviews/Reviews"));
 
